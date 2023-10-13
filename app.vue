4aa3d3db08479d33fad368a30d336df6c9d3c6e9
nemanjamalesija <malesija13@gmail.com>
2023-10-13 19:02:52 +0200

    Add todo list viewever example

--- app.vue
+++ app.vue
@@ -1,4 +1,36 @@
-<script setup lang="ts"></script>
-<template>app</template>
+<script lang="ts">
+import { defineNuxtComponent } from 'nuxt/app';
 
-<template></template>
+type TodoType = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [] as TodoType[],
+  }),
+
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then((response) => response.json())
+        .then((json) => (this.todoList = json));
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <img src="/todo.avif" alt="Todo photo" height="400px" />
+    <button @click="fetchTodoList">Fetch list</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" />
+        {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
