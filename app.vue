a07457f9620dbe8a2e0ca1a5267f40886191ec57
nemanjamalesija <malesija13@gmail.com>
2023-10-13 18:36:48 +0200

    Scafold project

---
+++ app.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>app</template>
+
+<template></template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import type { NuxtConfig } from '@nuxt/types';
+
+const config: NuxtConfig = {
+  buildModules: ['@nuxt/typescript-build'],
+  devtools: { enabled: true },
+};
+
+export default config;
